0023ade15730406f852412f8ce7bbd5506378428
Zameel Hassan <43750093+zameel7@users.noreply.github.com>
2023-02-15 09:04:07 +0530

    Update pass1.c

--- Pass 1/pass1.c
+++ Pass 1/pass1.c
@@ -12,8 +12,8 @@ void main() {
     // open files of input, output, symtab and optab
     f1 = fopen("input.txt", "r");
     f2 = fopen("optab.txt", "r");
-    f3 = fopen("output.txt", "w");
-    f4 = fopen("symtab.txt", "w");
+    f3 = fopen("symtab.txt", "w");
+    f4 = fopen("output.txt", "w");
 
     // accept the first input from input file
     fscanf(f1, "%s%s%s", label, opcode, operand);
@@ -24,7 +24,7 @@ void main() {
     {
         start_address = atoi(operand);
         locctr = start_address;
-        fprintf(f3, "\t%s\t%s\t%s", label, opcode, operand);
+        fprintf(f4, "\t%s\t%s\t%s", label, opcode, operand);
         fscanf(f1, "%s%s%s", label, opcode, operand);
     }
     else {
@@ -37,9 +37,9 @@ void main() {
         // print the first location counter
         fprintf(f4, "%d\t", locctr);
         // check if there is a symbol, if yes print it in the symtab else reject
-        if (strcmp(label, "-")==0)
+        if (strcmp(label, "-")!=0)
         {
-            fprintf(f3, "%s%s", label, locctr);
+            fprintf(f3, "%s  %s", label, locctr);
         }
         // go through the optab
         fscanf(f2, "%s\t%s", code, mnemonics);
